fa1fb48f2b41b83e346461ef250d59662fba40f0
Ramdan <dadanisme@upi.edu>
2025-11-01 00:17:46 +0800

    try this?

--- frontend/src/utils/supabase/client.ts
+++ frontend/src/utils/supabase/client.ts
@@ -1,8 +1,8 @@
 import { Database } from '@/types/database';
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
 export const createClient = () =>
-  createBrowserClient<Database>(supabaseUrl!, supabaseKey!);
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
